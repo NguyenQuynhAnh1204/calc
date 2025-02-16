fb7c8081b81ce9ab2e21d401abd5df0cd9a4ea30
Nguyễn Văn Bình - BinhNV1 <binhnv1@BRAVO.COM>
2025-02-16 21:41:30 +0700

    feat: create behavior base logic for calc

--- main.js
+++ main.js
@@ -1,25 +1,91 @@
-const buttInt = Array.from(document.getElementsByClassName('butt-int'))
+const buttsInt = Array.from(document.getElementsByClassName('butt-int'));
+const buttsMath = Array.from(document.getElementsByClassName('butt-math'));
+const expressionEl = document.getElementById('expression');
 
-const expression = document.getElementById("expression")
+//definition const for btn math name
+const PLUS = 'plus';
+const ABSOLUTE = 'absolute';
+const MINUS = 'minus';
+const CLEAR = 'clear';
 
-console.log("", buttInt)
-console.log("", expression)
+//definition const for btn math symbol
+const PLUS_SYMBOL = '+';
+const WhITE_SPACE = ' ';
 
-buttInt.forEach(function (butt, index) {
-    butt.onclick = () => { 
-        if (index < 3) {
-            console.log(index + 7);
-        } 
-        else if (index < 6) {
-            console.log(index + 1);
-        } 
-        else if (index < 9) {
-            console.log(index - 5);
-        } 
-        else if (index < 10) {
-            console.log(index - 9);
-        }
-     }
+//
+let expression = '';
+
+//add events click btns number pad:
+if (buttsInt.length > 0 && buttsInt.length <= 10) {
+	buttsInt.forEach(item => {
+		item.addEventListener('click', events => {
+			onClickBtnNumbersPad(events, item);
+		});
+	});
+} else console.warn('Please check number pad!!');
+
+//add events keydown number pad ...
+
+//add events click for btns Math
+
+buttsMath.forEach(btn => {
+	btn.addEventListener('click', eventArgs => {
+		onClickBtnMath(eventArgs, btn);
+	});
+});
+
+//add events keydown maths...
+
+function onClickBtnNumbersPad(eventArgs, button) {
+	if (!button) return;
+	const number = button.getAttribute('value');
+	expression += number;
+	updateExpressionElement();
 }
-)
 
+function onClickBtnMath(eventArgs, button) {
+	if (!button) return;
+	const math = button.getAttribute('value');
+
+	switch (math) {
+		case PLUS:
+			const isValid = validateNextMath(math);
+			if (!isValid) break;
+			expression = expression.trim(); // always remove leading spaces and terminate space in expression when add math symbol
+			expression += WhITE_SPACE;
+			expression += PLUS_SYMBOL;
+			expression += WhITE_SPACE;
+			break;
+		case MINUS:
+			validateNextMath();
+			break;
+
+		case CLEAR:
+			expression = '';
+			break;
+		default:
+			break;
+	}
+	updateExpressionElement();
+}
+
+function validateNextMath(pSymbol) {
+	//check something
+	let isValid = false;
+	switch (pSymbol) {
+		case PLUS:
+			let temp = expression.trimEnd();
+			const lastCharacter = temp[temp.length - 1];
+			isValid = lastCharacter !== PLUS_SYMBOL;
+			break;
+		default:
+			break;
+	}
+
+	return isValid;
+}
+
+function updateExpressionElement() {
+	if (!expressionEl) return;
+	expressionEl.innerText = expression;
+}
